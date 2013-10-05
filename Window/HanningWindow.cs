db8f22c25696426559adec4e91fb6813a9376012
aoisensi <aoisensi@aoisensi.info>
2013-10-05 10:28:11 +0900

    Add Window Function

---
+++ Window/HanningWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveLibrary.Window
+{
+    public class HanningWindow : Window
+    {
+        public HanningWindow(IWave wave)
+        {
+            this.wave = wave;
+        }
+
+        protected override double Func(double x)
+        {
+            return 0.5 - (Math.Cos(Math.PI * x * 2.0) * 0.5);
+        }
+    }
+}
---
+++ Window/Window.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveLibrary.Window
+{
+    public abstract class Window : IWave
+    {
+        protected IWave wave;
+
+        public int SamplingRate
+        {
+            get { return wave.SamplingRate; }
+        }
+
+        public int Length
+        {
+            get { return wave.Length; }
+        }
+
+        public double Read(int n)
+        {
+            return Func(((double)n) / Length) * wave.Read(n);
+        }
+
+        public IEnumerable<double> Reads()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                yield return Read(i);
+            }
+            yield break;
+        }
+
+        protected virtual double Func(double x)
+        {
+            return 1.0;
+        }
+    }
+}
